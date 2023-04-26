15bab8496c7f4daf2088219e293a99c9f876e181
paawandesai <82396021+paawandesai@users.noreply.github.com>
2023-04-25 23:18:32 -0500

    update

---
+++ PA7/AuctionItem.java
@@ -0,0 +1,122 @@
+package auction;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Timer;
+
+import auction.Server.AuctionItemObserver;
+
+public class AuctionItem {
+    Integer auctionItemId;
+    String name;
+    String description;
+    String startPrice;
+    String highestBid;
+    private boolean closed;
+    private boolean sold;
+    private String highestBidderId;
+    private int delay = 300000; // 5 minutes
+    private Timer timer;
+    private List<AuctionItemObserver> observers = new ArrayList<>();
+
+    public AuctionItem(Integer auctionItemId, String name, String description, String startPrice) {
+        this.auctionItemId = auctionItemId;
+        this.name = name;
+        this.description = description;
+        this.startPrice = startPrice;
+        this.highestBid = startPrice;
+        this.highestBidderId = null;
+        this.sold = false;
+    }
+
+    public Integer getAuctionItemId() {
+        return auctionItemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+    	this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+    	this.description = description;
+    }
+
+    public String getStartPrice() {
+        return startPrice;
+    }
+    public void setStartPrice(String startPrice) {
+    	this.startPrice = startPrice;
+    }
+
+    public String getHighestBid() {
+        return highestBid;
+    }
+
+    public void setHighestBid(String highestBid) {
+        this.highestBid = highestBid;
+    }
+
+    public String getHighestBidderId() {
+        return highestBidderId;
+    }
+    public void registerObserver(AuctionItemObserver observer) {
+        observers.add(observer);
+    }
+
+    public void unregisterObserver(AuctionItemObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (AuctionItemObserver observer : observers) {
+            observer.onUpdate(this);
+        }
+    }
+
+    public synchronized boolean bid(String customerId, String bidAmount) {
+        double amount = Double.parseDouble(bidAmount);
+        if (closed() || amount <= Double.parseDouble(highestBid) || amount <= Double.parseDouble(startPrice)) {
+            return false;
+        }
+        highestBid = bidAmount;
+        highestBidderId = customerId;
+        timer.restart();
+        return true;
+    }
+
+    public synchronized void closeAuction() {
+        if (!closed()) {
+            sold = true;
+            timer.stop();
+            System.out.println("Auction for item " + auctionItemId + " closed. Sold to customer " + highestBidderId + " for " + highestBid);
+        }
+    }
+
+    public synchronized boolean closed() {
+        return sold || !timer.isRunning();
+    }
+
+    public synchronized int getTimeLeft() {
+        if (closed()) {
+            return 0;
+        }
+        return (int) (delay - timer.getDelay()) / 1000;
+    }
+
+    public synchronized void setTimeLeft(int seconds) {
+        int timeLeft = (seconds * 1000) - (delay - timer.getDelay());
+        if (timeLeft > 0) {
+            timer.setDelay(timeLeft);
+        }
+    }
+
+}
---
+++ PA7/ClientHandler.java
@@ -0,0 +1,70 @@
+package auction;
+
+import java.awt.TrayIcon.MessageType;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Observer;
+
+import auction.Server.AuctionItemObserver;
+import javafx.application.Platform;
+
+import java.util.List;
+import java.util.Observable;
+
+class ClientHandler implements Runnable, Observer {
+
+	private Server server;
+	private Socket clientSocket;
+	private BufferedReader fromClient;
+	private PrintWriter toClient;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+
+	protected ClientHandler(Server server, Socket clientSocket) throws IOException {
+		this.server = server;
+		this.clientSocket = clientSocket;
+		BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter toServer = new PrintWriter(clientSocket.getOutputStream());
+
+		fromClient = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+		toClient = new PrintWriter(this.clientSocket.getOutputStream());
+
+	}
+
+	public void sendToClient(Message message) {
+		System.out.println("Sending to client: " + message);
+		toClient.println(message);
+		toClient.flush();
+	}
+
+	@Override
+	public void run() {
+		String input;
+		try {
+			while ((input = fromClient.readLine()) != null) {
+				System.out.println("[CH]: " + input);
+				server.processRequest(input, this);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// @Override
+	// public void onUpdate(AuctionItem item) {
+	// 	// TODO Auto-generated method stub
+
+	// }
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
